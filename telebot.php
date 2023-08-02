4245e0862cdacea0c6196e1d61942d6a20ff17b1
Aqil Mubarak <137961494+aqilmubarak@users.noreply.github.com>
2023-08-02 12:23:43 +0700

    Add files via upload

---
+++ telebot.php
@@ -0,0 +1,41 @@
+<?php
+/*===[PHP Setup]==============================================*/
+error_reporting(0);
+ini_set('display_errors', 0);
+
+/*===[Security Setup]=========================================*/
+include 'config.php';
+if ($_GET['referrer'] != "akahiko") { 
+    $i = rand(0,sizeof($red_link));
+    header("location: $red_link[$i]");
+    exit();
+}
+
+/*===[Variable Setup]=========================================*/
+$telebot = $_GET['telebot'];
+
+/*===[CC Info Validation]=====================================*/
+if($telebot == ""){
+    exit();
+}
+
+
+$keyboard = array(
+	"inline_keyboard" => array(
+		array(
+			array("text" => "Visit akahiko Checker", "url" => "https://akahiko.me/")
+		),
+		array(
+			array("text" => "Contact akahiko", "url" => "https://t.me/scssid")
+		)
+	)
+);
+$keyboard = json_encode($keyboard, true);
+$message = '<b>'.$bot_name.'</b>%0A%0ATest Complete, You may now continue checking cards with our checker.';
+phccoderSendMessage($message,$keyboard);
+
+/*===[PHP Functions]==========================================*/
+function phccoderSendMessage($message, $keyboard){
+    $url = $GLOBALS['token_url']."/sendMessage?chat_id=".$GLOBALS['telebot']."&text=".$message."&parse_mode=HTML&reply_markup=".$keyboard;
+    file_get_contents($url); 
+}
